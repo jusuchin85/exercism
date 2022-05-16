b2c7809630c634341ab759bf50dbadd235ee4bb5
Justin Alex Paramanandan <1155821+jusuchin85@users.noreply.github.com>
2022-05-16 10:13:07 +1000

    Define solution for step 1 Implemented step 1 - which is one that performs addition, multiplication and division successfully.
    Might consider refactoring into multiple methods? Not sure. Will revisit
    when I finish the entire exercise set.

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,23 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
+  @result = nil
 
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    if ALLOWED_OPERATIONS.include? operation
+     if operation == "+"
+        @result = first_operand + second_operand
+     elsif operation == "*"
+        @result = first_operand * second_operand
+     else
+       @result = first_operand / second_operand
+     end
+     "#{first_operand} #{operation} #{second_operand} = #{@result}"
+    else
+      "bye"
+    end
   end
 end
+
+# puts SimpleCalculator.calculate(16, 51, "+")
+# puts SimpleCalculator.calculate(32, 6, "*")
+# puts SimpleCalculator.calculate(512, 4, "/")
