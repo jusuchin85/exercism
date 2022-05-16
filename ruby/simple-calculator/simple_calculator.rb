75342a7c44f37d6d28a3a3b7ae6436003757cf62
Justin Alex Paramanandan <1155821+jusuchin85@users.noreply.github.com>
2022-05-16 11:58:39 +1000

    Add catch to ArgumentError exception Implement handling of invalid arguments. Perhaps a better way to handle this?

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,36 +1,40 @@
 class SimpleCalculator
+  class UnsupportedOperation < StandardError
+  end
 
-  class UnsupportedOperation < StandardError; end
-
-  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
+  ALLOWED_OPERATIONS = %w[+ / *].freeze
   @result = nil
 
   def self.calculate(first_operand, second_operand, operation)
+    if (!first_operand.is_a? Integer) || (!second_operand.is_a? Integer)
+      raise ArgumentError
+    end
+
     if ALLOWED_OPERATIONS.include? operation
-     if operation == "+"
+      if operation == "+"
         @result = first_operand + second_operand
-     elsif operation == "*"
+      elsif operation == "*"
         @result = first_operand * second_operand
-     else
-       @result = first_operand / second_operand
-     end
-     "#{first_operand} #{operation} #{second_operand} = #{@result}"
+      else
+        @result = first_operand / second_operand
+      end
+      "#{first_operand} #{operation} #{second_operand} = #{@result}"
     else
       raise UnsupportedOperation
     end
   end
 end
 
-## Step 1
+# # Step 1
 # puts SimpleCalculator.calculate(16, 51, "+")
 # puts SimpleCalculator.calculate(32, 6, "*")
 # puts SimpleCalculator.calculate(512, 4, "/")
 
-# Step 2
-puts SimpleCalculator.calculate(1, 2, '-')
+# # Step 2
+# puts SimpleCalculator.calculate(1, 2, '-')
 
 # # Step 3
-# puts SimpleCalculator.calculate(1, '2', '*')
+# puts SimpleCalculator.calculate(1, "2", "*")
 
 # # Step 4
 # puts SimpleCalculator.calculate(512, 0, "/")
