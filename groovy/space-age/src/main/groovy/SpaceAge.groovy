63b8f39e85eb2a74c68863d883c1924e0f1a2ca9
Justin Alex Paramanandan <1155821+jusuchin85@users.noreply.github.com>
2022-06-13 23:36:31 +1000

    Update Space Age exercise
    So, found a cleaner way to:
    
    - define a map
    - call a map without declaring a variable to store a map-find

--- groovy/space-age/src/main/groovy/SpaceAge.groovy
+++ groovy/space-age/src/main/groovy/SpaceAge.groovy
@@ -1,21 +1,20 @@
 class SpaceAge {
 
     static planets = [
-        1: [name: 'Mercury', period: 0.2408467],
-        2: [name: 'Venus', period: 0.61519726],
-        3: [name: 'Earth', period: 1],
-        4: [name: 'Mars', period: 1.8808158],
-        5: [name: 'Jupiter', period: 11.862615],
-        6: [name: 'Saturn', period: 29.447498],
-        7: [name: 'Uranus', period: 84.016846],
-        8: [name: 'Neptune', period: 164.79132]
+        "Mercury" : 0.2408467,
+        "Venus" : 0.61519726,
+        "Earth" : 1,
+        "Mars" : 1.8808158,
+        "Jupiter" : 11.862615,
+        "Saturn" : 29.447498,
+        "Uranus" : 84.016846,
+        "Neptune" : 164.79132
     ]
 
-    static int SECONDS_IN_EARTH = 31557600
+    static SECONDS_IN_EARTH = 31557600
 
     static double age(String planet, int seconds) {
-        def thePlanet = planets.find { it.value.name == planet }
-        ((seconds / SECONDS_IN_EARTH) / thePlanet.value.period).round(2)
+        (seconds/SECONDS_IN_EARTH/planets[planet]).round(2)
     }
 
 }
